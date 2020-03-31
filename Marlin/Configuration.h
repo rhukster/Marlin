506db5be9ea8f520ceb429785f472389c3281337
Andy Miller <rhuk@mac.com>
2020-03-30 20:03:08 -0600

    Revert "ESP3D Configuration"
    This reverts commit 7b88060f4789108f66bee7e0ee7f0ee4c8717c5d.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -94,7 +94,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 6
+#define SERIAL_PORT -1
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
