2d322ab25068bc8125a62bc6286b4adb46cfff2c
Andy Miller <rhuk@mac.com>
2020-05-24 17:59:39 -0600

    move origin to front left (also home)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -609,10 +609,10 @@
 
 // Uncomment one of these options to enable CoreXY, CoreXZ, or CoreYZ kinematics
 // either in the usual order or reversed
-#define COREXY
+// #define COREXY
 //#define COREXZ
 //#define COREYZ
-// #define COREYX
+#define COREYX
 //#define COREZX
 //#define COREZY
 
@@ -1073,7 +1073,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 
 // @section extruder
