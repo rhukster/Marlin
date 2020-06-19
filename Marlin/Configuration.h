6d4ca95b17e03c0d9eb3e370e178303c855e3f22
Andy Miller <rhuk@mac.com>
2020-06-19 14:24:27 -0600

    lower xy max speeds

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -756,7 +756,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 400, 8.6, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 250, 250, 8.6, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
