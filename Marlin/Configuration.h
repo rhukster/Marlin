657a0e97b60f255caf374070c819abdfb6d8ef0a
Andy Miller <rhuk@mac.com>
2021-01-17 00:00:54 -0700

    petg z-offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -985,7 +985,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -10, -.75 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -10, -.76 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
